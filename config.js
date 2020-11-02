417fde8f4b6c09fe9811b5a542b88049d5105e64
5e34d362a834270cb09987e4 <leah.borns@gmail.com>
2020-11-02 12:00:06 -0800

    updated to mongodb atlas

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     (process.env.NODE_ENV === 'production' ?
-     'mongodb://admin:admin@ds237717.mlab.com:37717/commute-calculator-final-capstone' :
-     'mongodb://admin:admin@ds237717.mlab.com:37717/commute-calculator-final-capstone');
+     'mongodb+srv://LeahBorns:Test1234@commute-calculator-fina.zpygx.mongodb.net/commute-calculator-final-capstone?retryWrites=true&w=majority' :
+     'mongodb+srv://LeahBorns:Test1234@commute-calculator-fina.zpygx.mongodb.net/commute-calculator-final-capstone?retryWrites=true&w=majority');
 exports.PORT = process.env.PORT || 8080;
 
 //exports.JWT_SECRET = process.env.JWT_SECRET;
 //exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '365d';
 
 exports.JWT_SECRET = process.env.JWT_SECRET;
-DATABASE_URL = 'mongodb://admin:admin@ds237717.mlab.com:37717/commute-calculator-final-capstone'
+DATABASE_URL = 'mongodb+srv://LeahBorns:Test1234@commute-calculator-fina.zpygx.mongodb.net/commute-calculator-final-capstone?retryWrites=true&w=majority'
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
